00283015eb28c3024afe5c152692f1ca6a06349e
Romal Patel <romal@ravi.sharma>
2024-02-14 16:36:04 +0530

    update run.sh file

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Change directory to /var/www/my-app
-cd /var/www/my-app || exit
+cd /var/www/my-app
 
 # Copy .env file to dist directory
-cp .env dist/ || exit
-
-# Change directory to dist
-cd dist || exit
+cp .env dist/
 
 npm install pm2 -g
+
+# Change directory to dist
+cd dist
 
 # Start the application with pm2
 pm2 start index.js --name app --watch

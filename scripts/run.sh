9f6d39c2bc18b73253efc1b34c8e8add2c8efc2b
Romal Patel <romal@ravi.sharma>
2024-02-14 16:28:22 +0530

    add run sh file

---
+++ scripts/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Change directory to /var/www/my-app
+cd /var/www/my-app || exit
+
+# Copy .env file to dist directory
+cp .env dist/ || exit
+
+# Change directory to dist
+cd dist || exit
+
+# Start the application with pm2
+pm2 start index.js --name app --watch || exit

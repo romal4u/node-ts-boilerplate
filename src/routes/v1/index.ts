4dde4884844c460ec28943375e5b408846566b3b
Romal Patel <romal@ravi.sharma>
2024-02-14 15:01:20 +0530

    add status API

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,9 +1,11 @@
-import { Router } from 'express'
+import { Request, Response, Router } from 'express'
 
 import auth from './auth'
 
 const router = Router()
 
 router.use('/auth', auth)
+
+router.get('/status', (req: Request, res: Response) => res.send('OK'))
 
 export default router
